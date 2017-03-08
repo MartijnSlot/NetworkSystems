eb604dc8e482d3a14ed0726770b23237f1ae77ad
MartijnSlot <martijn.slot@nedap.com>
2017-03-08 20:04:56 +0100

    photo 5 score around 7

--- src/DRDTChallengeClientJava/src/protocol/HighlyEfficientDataTransferProtocol.java
+++ src/DRDTChallengeClientJava/src/protocol/HighlyEfficientDataTransferProtocol.java
@@ -8,10 +8,10 @@ public class HighlyEfficientDataTransferProtocol extends IRDTProtocol {
 
     // change the following as you wish:
     private static final int HEADERSIZE = 1;   // number of header bytes in each packet
-    private static final int DATASIZE = 200;   // max. number of user data bytes in each packet
-    private static final int TIMEOUT = 10000;
+    private static final int DATASIZE = 400;   // max. number of user data bytes in each packet
+    private static final int TIMEOUT = 8000;
     private static final int TIMES_TO_CHECK_FOR_ACK = 100;
-    private static final int WINDOW_SIZE = 8;
+    private static final int WINDOW_SIZE = 3;
 
     @Override
     public void sender() {
@@ -112,7 +112,7 @@ public class HighlyEfficientDataTransferProtocol extends IRDTProtocol {
         // handle expiration of the timeout:
         System.out.println("Timer expired with tag=" + z);
     }
-
+    //TODO netjes maken en acks terugsturen binnen de window die daarna niet meer verstuurd worden.
     @Override
     public void receiver() {
         System.out.println("Receiving...");
@@ -124,53 +124,52 @@ public class HighlyEfficientDataTransferProtocol extends IRDTProtocol {
         while (!stop) {
 
             Integer[] packet = getNetworkLayer().receivePacket();
+            Integer[] prematurePacket = new Integer[0];
 
             if (packet != null) {
-
-                int oldlength = fileContents.length;
-                int datalen = packet.length - HEADERSIZE;
-                boolean tooEarly = false;
-
-                System.out.println("Received packet, length=" + packet.length + "  first byte=" + packet[0]);
-
-                boolean previousPacketsReceived = true;
-                for (int i = 1; i < packet[0]; i++) {
-                    if (!receivedPackets.contains(i)) {
-                        previousPacketsReceived = false;
-                    }
-                }
 
                 if (receivedPackets.size() > 0) {
                     int j = Collections.max(receivedPackets);
                     if (packet[0] - j > 1) {
-                        tooEarly = true;
+                        prematurePacket = packet;
                         System.out.print("Packet " + packet[0] +
                                 " arrived too early. First wait for packet " + (packet[0] - 1) + "\n");
                     }
                 }
 
+                System.out.println("Received packet, length=" + packet.length + "  first byte=" + packet[0]);
 
                 /**
                  * send ACK packet with the right header back if all previous packets are received
-                 *
                  */
+                if (previousPacketsReceived(packet, receivedPackets) && !receivedPackets.contains(packet[0])) {
+                    int oldlength = fileContents.length;
+                    int datalen = packet.length - HEADERSIZE;
 
-                if (previousPacketsReceived && !tooEarly) {
-                    Integer[] ackPacket = createEmptyPacket(packet[0]);
-                    getNetworkLayer().sendPacket(ackPacket);
-                    System.out.println("ACK sent for packet: " + packet[0]);
-                    if (!receivedPackets.contains(packet[0])) {
+                    sendAck(packet);
+                    fileContents = Arrays.copyOf(fileContents, oldlength + datalen);
+                    System.arraycopy(packet, HEADERSIZE, fileContents, oldlength, datalen);
+                    System.out.print("Packet " + packet[0] +
+                            " has been added to the file on spot " + oldlength +
+                            " to " + (oldlength + datalen) + "\n");
+                    receivedPackets.add(packet[0]);
+                    if (packet.length < DATASIZE) {
+                        stop = true;
+                    }
+                    if (prematurePacket.length > 0) {
+                        sendAck(prematurePacket);
+                        oldlength = fileContents.length;
+                        datalen = packet.length - HEADERSIZE;
                         fileContents = Arrays.copyOf(fileContents, oldlength + datalen);
                         System.arraycopy(packet, HEADERSIZE, fileContents, oldlength, datalen);
                         System.out.print("Packet " + packet[0] +
                                 " has been added to the file on spot " + oldlength +
                                 " to " + (oldlength + datalen) + "\n");
                         receivedPackets.add(packet[0]);
+                        if (packet.length < DATASIZE) {
+                            stop = true;
+                        }
                     }
-                }
-
-                if (packet.length < DATASIZE) {
-                    stop = true;
                 }
 
             } else {
@@ -185,6 +184,24 @@ public class HighlyEfficientDataTransferProtocol extends IRDTProtocol {
         // write to the output file
         Utils.setFileContents(fileContents, getFileID());
     }
+
+    private boolean previousPacketsReceived(Integer[] packet, Set<Integer> receivedPackets) {
+        for (int i = 1; i < packet[0]; i++) {
+            if (!receivedPackets.contains(i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void sendAck(Integer[] packet) {
+        Integer[] ackPacket = createEmptyPacket(packet[0]);
+        getNetworkLayer().sendPacket(ackPacket);
+        System.out.println("ACK sent for packet: " + packet[0]);
+    }
+
+
+
 }
 
 
